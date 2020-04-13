de51a13dc96e7aef9a6967fa9526e324fdc16b3f
Konmoron <cqlq2012@gmail.com>
2020-04-13 15:23:00 +0800

    修改 UnittestPostJson 的参数

--- utils/unittest-utils.go
+++ utils/unittest-utils.go
@@ -65,7 +65,7 @@ func UnittestPostForm(uri string, param map[string]string, router *gin.Engine) (
 }
 
 // UnittestPostJSON 根据特定请求uri和参数param，以Json形式传递参数，发起post请求返回响应
-func UnittestPostJSON(uri string, param map[string]interface{}, router *gin.Engine) (*http.Response, []byte) {
+func UnittestPostJSON(uri string, param interface{}, router *gin.Engine) (*http.Response, []byte) {
 	// 将参数转化为json比特流
 	jsonByte, _ := json.Marshal(param)
 
